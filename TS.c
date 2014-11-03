fa14c158d4317d0e83a90addb78b72fd83111a14
Hamza5 <hamza.abbad@gmail.com>
2014-11-03 11:35:49 +0100

    Ajouter le type de l'entité et sa taille; modification de la fonction inserer

--- TS.c
+++ TS.c
@@ -1,7 +1,11 @@
 #include <string.h>
 #include <stdio.h>
 #include "TS.h"
-typedef struct elements {char nom[10]; char type; char nature; unsigned short taille;} element;
+typedef struct elements {char nom[9]; char type; char nature; unsigned short taille;} element;
+// nom : AND 25.5 (-2) ; Idf "Programme" 'Y' ...
+// type : K (Mot-clé), I (Identificateur), S (Séparateur), O (Opérateur), N (Entier), F (Décimal), C (Caractère), T (Chaîne de caractères)
+// nature
+// taille : en octets
 element ts[500]; // Le tableau de symbols
 unsigned short fin = 0;
 int rechercher(const char * entite){
@@ -10,13 +14,14 @@ int rechercher(const char * entite){
 	if(i==fin) return -1; // Entité non trouvée
 	else return i; // L'indice de l'entité
 }
-void inserer(const char * entite){
+void inserer(const char * entite, char type, unsigned short taille){
 	int position = rechercher(entite);
 	if(position==-1){ // Nouvelle entité
 		fin++;
-		strcpy(ts[fin].nom,entite); // Insérer l'entité
+		strcpy(ts[fin].nom,entite); // Insérer le nom de l'entité
+		ts[fin].type = type; // Insérer le type de l'entité
+		ts[fin].taille = taille; // Insérer la taille de l'entité
 	}
-	else{}
 }
 void afficher(){
 	printf("------------\n");
--- TS.h
+++ TS.h
@@ -1,6 +1,6 @@
 #ifndef TS
 #define TS
 int rechercher(const char * entite);
-void inserer(const char * entite);
+void inserer(const char * entite, char type, unsigned short taille);
 void afficher();
 #endif
